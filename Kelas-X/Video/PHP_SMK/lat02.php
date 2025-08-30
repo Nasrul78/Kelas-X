b896ac5b290bac10f464807b6d336af16d2e8fc9
Nasrul <122144023+Nasrul78@users.noreply.github.com>
2025-08-30 14:39:14 +0700

    PHP Eps. 03

---
+++ Kelas-X/Video/PHP_SMK/lat02.php
@@ -0,0 +1,11 @@
+<?php
+    $tulisan = "Saya belajar PHP";
+    echo $tulisan, "<br>";
+
+    $angka = 2575;
+    echo "Tahun ", $angka, "<br>";
+
+    var_dump($tulisan);
+    echo "<br>";
+    var_dump($angka);
+?>
