876eaf287619d01bc84e5e0f680b3137d4816dd8
Nasrul <122144023+Nasrul78@users.noreply.github.com>
2025-09-01 00:16:02 +0700

    PHP Eps. 19

--- Kelas-X/Video/PHP_SMK/restoran/kategori/select.php
+++ Kelas-X/Video/PHP_SMK/restoran/kategori/select.php
@@ -1,7 +1,29 @@
+<div style="margin: auto; width: 900px;">
+
 <?php 
     require_once "../function.php";
 
-    $sql = "SELECT * FROM tblkategori";
+    $sql = "SELECT idkategori FROM tblkategori";
+    $result = mysqli_query($koneksi, $sql);
+    $jumlahData = mysqli_num_rows($result);
+
+    $banyak = 3;
+    $halaman = ceil($jumlahData / $banyak);
+
+    for ($i=1; $i <= $halaman; $i++) { 
+        echo '<a href="?p='.$i.'">'.$i.'</a>'."&nbsp &nbsp &nbsp";
+    }
+
+    echo "<br><br>";
+
+    if ( isset($_GET["p"]) ) {
+        $p = $_GET["p"];
+        $mulai = ($p - 1) * $banyak;
+    } else {
+        $mulai = 0;
+    }
+    
+    $sql = "SELECT * FROM tblkategori LIMIT $mulai, $banyak";
     $result = mysqli_query($koneksi, $sql);
     // var_dump($result);
 
@@ -16,10 +38,12 @@
     </tr>
     ';
 
+    $no = $mulai + 1;
+
     if ($jumlah > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             echo "<tr>";
-            echo "<td>", $row["idkategori"], "</td>";
+            echo "<td>", $no++, "</td>";
             echo "<td>", $row["kategori"], "</td>";
             echo "</tr>";
         }
@@ -27,3 +51,5 @@
 
     echo "</table>";
 ?>
+
+</div>
