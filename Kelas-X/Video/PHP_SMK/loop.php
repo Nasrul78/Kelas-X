4e6f0cd987e509cc5806eba7c9ad22fc16265cb2
Nasrul <122144023+Nasrul78@users.noreply.github.com>
2025-08-31 07:45:09 +0700

    PHP Eps. 08

---
+++ Kelas-X/Video/PHP_SMK/loop.php
@@ -0,0 +1,27 @@
+<?php 
+    for ($i=1; $i < 12; $i += 2) { 
+        echo $i, ", ";
+    }
+
+    echo "<br>";
+
+    for ($i=12; $i >= 1; $i -= 2) { 
+        echo $i, ", ";
+    }
+
+    echo "<br>";
+
+    $a = 1;
+    while ($a <= 100) {
+        echo $a, ", ";
+        $a++;
+    }
+
+    echo "<br>";
+
+    $b = 1;
+    do {
+        echo $b, ", ";
+        $b++;
+    } while ($b <= 100);
+?>
