86aefbb2c85a9c0d8354f98f2d0bc4136bf092de
Nasrul <122144023+Nasrul78@users.noreply.github.com>
2025-09-01 20:03:19 +0700

    PHP Eps. 42

--- Kelas-X/Video/PHP_SMK/restoran/index.php
+++ Kelas-X/Video/PHP_SMK/restoran/index.php
@@ -6,6 +6,11 @@
 
     $sql = "SELECT * FROM tblkategori ORDER BY kategori";
     $row = $db->getALL($sql);
+
+    if (isset($_GET["log"])) {
+        session_destroy();
+        header("location:index.php");
+    }
 ?>
 
 <!DOCTYPE html>
@@ -26,12 +31,12 @@
                 <?php 
                     if (isset($_SESSION["pelanggan"])) {
                         echo '
-                            <div class="float-end mt-4">Logout</div>
-                            <div class="float-end mt-4 me-4">Pelanggan</div>
+                            <div class="float-end mt-4"><a href="?log=logout">Logout</a></div>
+                            <div class="float-end mt-4 me-4">Pelanggan: '.$_SESSION["pelanggan"].'</div>
                         ';
                     } else {
                         echo '
-                            <div class="float-end mt-4 me-4">Login</div>
+                            <div class="float-end mt-4 me-4"><a href="?f=home&m=login">Login</a></div>
                             <div class="float-end mt-4 me-4"><a href="?f=home&m=daftar">Daftar</a></div>
                         ';
                     }
