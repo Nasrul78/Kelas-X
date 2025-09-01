43f9153eaa54fdc61c3143681030be5a1ef43388
Nasrul <122144023+Nasrul78@users.noreply.github.com>
2025-09-01 09:43:25 +0700

    PHP Eps. 25

---
+++ Kelas-X/Video/PHP_SMK/restoran/admin/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main Page | Aplikasi Restoran SMK</title>
+    <link rel="stylesheet" href="../bootstrap-5.3.7-dist/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <h2>Restoran</h2>
+            </div>
+            <div class="col-md-9">
+                <div class="float-end mt-4">Logout</div>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-md-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Kategori</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Pelanggan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Order</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Order Detail</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">User</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-9">
+
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col">
+                <p class="text-center">2025 - copyright@smkn2buduran.com</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
