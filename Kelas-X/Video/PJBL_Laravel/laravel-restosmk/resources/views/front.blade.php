ff16105551d13942fd6066535ac74c1462ec3601
Nasrul <122144023+Nasrul78@users.noreply.github.com>
2025-09-02 23:45:37 +0700

    Laravel Restoran Eps. 8

--- Kelas-X/Video/PJBL_Laravel/laravel-restosmk/resources/views/front.blade.php
+++ Kelas-X/Video/PJBL_Laravel/laravel-restosmk/resources/views/front.blade.php
@@ -5,8 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Aplikasi Restoran SMK</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
 </head>
 <body>
-    <h1>Project Aplikasi Restoran</h1>
+    <div class="container">
+        <div class="mt-2">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a href="#"><img style="width: 200px;" src="{{ asset('gambar/logo.png') }}" alt=""></a>
+                    <ul class="navbar-nav gap-5">
+                        <li class="nav-item">Cart</li>
+                        <li class="nav-item">Register</li>
+                        <li class="nav-item">Email</li>
+                        <li class="nav-item">Login</li>
+                        <li class="nav-item">Logout</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div>
+            <div>
+                Kategori
+            </div>
+            <div>
+                Content
+            </div>
+        </div>
+        <div>
+            Footer
+        </div>
+    </div>
+
+    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
