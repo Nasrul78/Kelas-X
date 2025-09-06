783c9b16a8568d410ea67d98044d09c6c1f4d6ec
Nasrul <122144023+Nasrul78@users.noreply.github.com>
2025-09-06 11:37:20 +0700

    Javascript Eps. 4

---
+++ Kelas-X/Video/Javascript/js/operator.js
@@ -0,0 +1,43 @@
+let x = 2;
+x = -x;
+console.log(x);
+
+let p = 10, l = 7, t = 3, volume;
+volume = p * l * t;
+console.log(volume);
+
+console.log(4 + 6);
+console.log(4 - 6);
+console.log(4 * 6);
+console.log(4 / 6);
+console.log(4 % 6);
+
+let kota = "sido" + "arjo";
+console.log(kota);
+
+console.log("2" + 3 + 4);
+console.log(2 + 3 + "4");
+
+console.log(5 - "3");
+console.log("5" - "3");
+
+console.log("5" / "3");
+console.log(5 / "3");
+console.log("5" / 3);
+
+console.log(7 ** 2);
+
+let a, b, c;
+a = b = c = 4 + 5;
+console.log(c);
+
+let inc = 4;
+inc++;
+console.log(inc);
+
+inc--;
+console.log(inc);
+
+let dec = 2;
+dec--;
+console.log(dec);
