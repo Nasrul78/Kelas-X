1a855474dabf573d4abd4af27a978e9970547082
Nasrul <122144023+Nasrul78@users.noreply.github.com>
2025-09-18 09:47:58 +0700

    Javascript Eps. 27

---
+++ Kelas-X/Video/Javascript/js/json.js
@@ -0,0 +1,22 @@
+let tblmenu = [
+  {
+    idmenu: 1,
+    idkategori: 1,
+    menu: "Apel Manalagi",
+    harga: 3000,
+    warna: ["merah", "kuning", "hijau"],
+    stock: true,
+    keterangan: null,
+  },
+  {
+    idmenu: 2,
+    idkategori: 1,
+    menu: "Jeruk Bayi",
+    harga: 5000,
+    warna: ["oranye"],
+    stock: true,
+    keterangan: null,
+  },
+]
+
+console.log(tblmenu[0].menu)
