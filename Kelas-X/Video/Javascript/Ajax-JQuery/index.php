6b6b3008bbda264d3e6319214e710921152d5417
Nasrul <122144023+Nasrul78@users.noreply.github.com>
2025-09-23 11:49:40 +0700

    Javascript Eps. 35

--- Kelas-X/Video/Javascript/Ajax-JQuery/index.php
+++ Kelas-X/Video/Javascript/Ajax-JQuery/index.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -7,9 +7,55 @@
     <title>Ajax-JQuery Bootstrap PHP</title>
 
     <script src="js/jquery.js"></script>
+    <script src="js/app.js"></script>
   </head>
-  <body>
-    <h1>Belajar Ajax-JQuery Bootstrap PHP</h1>
+  <body>    
+    <div class="container">
+      <div class="row">
+        <h1>Belajar Ajax-JQuery Bootstrap PHP</h1>
+      </div>
+      <div class="mt-4 row">
+        <div class="col-sm">
+          <div class="row">
+            <h2>Input Data Pelanggan</h2>
+          </div>
+          <form>
+            <div class="mb-3">
+              <label for="pelanggan" class="form-label">Pelanggan</label>
+              <input type="text" class="form-control" id="pelanggan" aria-describedby="emailHelp">
+              <div id="emailHelp" class="form-text">Harus diisi!</div>
+            </div>
+            <div class="mb-3">
+              <label for="alamat" class="form-label">Alamat</label>
+              <input type="text" class="form-control" id="alamat">
+              <div id="emailHelp" class="form-text">Harus diisi!</div>
+            </div>
+            <div class="mb-3">
+              <label for="telp" class="form-label">Telp</label>
+              <input type="text" class="form-control" id="telp">
+              <div id="emailHelp" class="form-text">Harus diisi!</div>
+            </div>
+            <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+        <div class="col-sm">
+          <div class="row"><h2>Data Pelaggan</h2></div>
+          <div class="row">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">No</th>
+                  <th scope="col">Pelanggan</th>
+                  <th scope="col">Alamat</th>
+                  <th scope="col">Telp</th>
+                </tr>
+              </thead>
+              <tbody id="isiData"></tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
