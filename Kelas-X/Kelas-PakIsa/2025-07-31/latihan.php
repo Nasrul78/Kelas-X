adfde7b4eeb49446caae9f72f2ba796b9024ec75
Nasrul78 <nasrulillah78@gmail.com>
2026-01-08 07:14:42 +0700

    hapus folder lama

--- Kelas-X/Kelas-PakIsa/2025-07-31/latihan.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "saya belajar PHP";
--- Kelas-X/Kelas-PakIsa/2025-10-23/user.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    if (!isset($_SESSION['email'])){
-        header("location: index.php");
-    }
-?>
-
-<h1>Selamat datang <?= $_SESSION ['email'] ?></h1>
